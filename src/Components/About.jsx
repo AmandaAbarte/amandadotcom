185e323c8ef5b0bf27dd2545839c034263f61676
AmandaAbarte <amanda.abarte@gmail.com>
2022-05-19 14:14:27 +0200

    rotating profile pictures in about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import prof1 from "../assets/profile(1).png";
+import prof2 from "../assets/profile(2).png";
+import prof3 from "../assets/profile(3).png";
+import prof4 from "../assets/profile(4).png";
 
 export default function About () {
     return (
         <div className="about-container">
-            {/* <h1>this is about me</h1> */}
+            <div className="border">
+                <h1>Amanda Abarte</h1>
+                <h2>Front-end Dev, Designer, Artist</h2>
+                <div className="profile">
+                    <img src={prof1} className="prof prof1"></img>
+                    <img src={prof2} className="prof prof2"></img>
+                    <img src={prof3} className="prof prof3"></img>
+                    <img src={prof4} className="prof prof4"></img>
+                </div>
+            </div>
         </div>
     )
 }
