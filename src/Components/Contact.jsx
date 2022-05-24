3271a885daebc2bcc734a03a0c3d81ebe3cc06f9
AmandaAbarte <amanda.abarte@gmail.com>
2022-05-24 11:16:46 +0200

    contact form styling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -8,25 +8,28 @@ export default function Contact(props) {
     isHuman: false,
   });
   function handleInputs(event) {
-      const {name,value, type, checked} = event.target;
-      setFormData(prevData => {
-          return {
-              ...prevData,
-              [name]: type === "checkbox" ? checked : value
-          }
-      })
+    const { name, value, type, checked } = event.target;
+    setFormData((prevData) => {
+      return {
+        ...prevData,
+        [name]: type === "checkbox" ? checked : value,
+      };
+    });
   }
   function handleSubmit(event) {
-      event.preventDefault();
-      //send to api?
-      console.log(formData);
+    event.preventDefault();
+    //send to api?
+    console.log(formData);
   }
   return (
     <div
       className="contact-container"
-      style={{ backgroundColor: props.lights ? "white" : "black" }}
+      style={{
+        backgroundColor: props.lights ? "white" : "black",
+      }}
     >
       <form className="form" onSubmit={handleSubmit}>
+        <h1> Get in Touch</h1>
         <input
           type="text"
           placeholder="First and Last Name"
@@ -35,6 +38,10 @@ export default function Contact(props) {
           value={formData.name}
           required
           className="name"
+          style={{
+            color: props.lights ? "black" : "white",
+            borderBottom: props.lights ? "1px solid black" : "1px solid white",
+          }}
         />
         <input
           type="email"
@@ -44,6 +51,10 @@ export default function Contact(props) {
           value={formData.email}
           required
           className="email"
+          style={{
+            color: props.lights ? "black" : "white",
+            borderBottom: props.lights ? "1px solid black" : "1px solid white",
+          }}
         />
         <input
           type="text"
@@ -53,15 +64,19 @@ export default function Contact(props) {
           value={formData.message}
           required
           className="message"
+          style={{
+            color: props.lights ? "black" : "white",
+            borderBottom: props.lights ? "1px solid black" : "1px solid white",
+          }}
         />
         <input
-            type="checkbox"
-            id= "isHuman"
-            checked={formData.isHuman}
-            onChange={handleInputs}
-            name="isHuman"
-            required
-            className="checkbox"
+          type="checkbox"
+          id="isHuman"
+          checked={formData.isHuman}
+          onChange={handleInputs}
+          name="isHuman"
+          required
+          className="checkbox"
         />
         <label htmlFor="isHuman">Are you really human?</label>
         <button className="submit">Submit</button>
